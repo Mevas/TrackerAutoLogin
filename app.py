050254c324aa9a088b99929a73357604d154fabd
Alexandru Vasilescu <alexandru.vasilescu01@gmail.com>
2024-08-25 15:57:44 +0300

    Fix chrome_options arguments

--- app.py
+++ app.py
@@ -21,9 +21,14 @@ def set_chrome_options() -> None:
     """
 
     chrome_options = Options()
+    # Based on https://stackoverflow.com/a/52340526
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("enable-automation")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-browser-side-navigation")
+    chrome_options.add_argument("--disable-gpu")
     chrome_prefs = {}
     chrome_options.experimental_options["prefs"] = chrome_prefs
     ## Hide images from the browser
